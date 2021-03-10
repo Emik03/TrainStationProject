92831e5479547a67287519f3126646c57bc5cc70
Emik03 <emik.lakunishok@gmail.com>
2021-03-10 11:01:55 +0100

    API request implementation

--- TrainStation.py
+++ TrainStation.py
@@ -1,37 +1,84 @@
+import requests
 import tkinter as tk
+import json
+from tkinter import ttk
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 
-# Init
+# List of stations.
+stations = { 'Karlstad':'Ks', 'Arvika': 'Ar' }
+
+def search():
+    API_KEY = 'aa916ca49ce741d5bc78df1302631bfd'
+    URL = 'https://api.trafikinfo.trafikverket.se/v1.3/data.json'
+
+    # Creates the request.
+    request = f"""<REQUEST><LOGIN authenticationkey="{API_KEY}" /><QUERY objecttype="TrainAnnouncement" schemaversion="1.3" orderby="AdvertisedTimeAtLocation"><FILTER><AND><EQ name="ActivityType" value="Avgang" /><EQ name="LocationSignature" value="{stations[state.get()]}" /><OR><AND><GT name="AdvertisedTimeAtLocation" value="$dateadd(07:00:00)" /><LT name="AdvertisedTimeAtLocation" value="$dateadd(12:00:00)" /></AND></OR></AND></FILTER><INCLUDE>AdvertisedTrainIdent</INCLUDE><INCLUDE>AdvertisedTimeAtLocation</INCLUDE><INCLUDE>TrackAtLocation</INCLUDE><INCLUDE>ToLocation</INCLUDE></QUERY></REQUEST>"""
+
+    # This request is sent, we also have to specify that it's XML.
+    response = requests.post(URL, data = request, headers = {'Content-Type': 'text/xml'})
+
+    # This retrieves the information sent.
+    response_json = json.loads(response.text)
+    departures = response_json["RESPONSE"]['RESULT'][0]['TrainAnnouncement']
+
+    # We need to first reenable the text field before editing it.
+    txt.config(state="normal")
+
+    # We need to also clear it before we insert text.
+    txt.delete(1.0,"end")
+
+    # Writes the list.
+    for dep in departures:
+        txt.insert(1., dep)
+        txt.insert(1., '\n--------------------\n')
+    
+    # Disables it again, to make sure that it remains readonly.
+    txt.config(state="disabled")
+
+def save():
+    # Opens a prompt to save a file name.
+    filepath = asksaveasfilename(
+        defaultextension='txt',
+        filetypes=[('Text File', '*.txt'), ('All Files', '*.*')],
+    )
+
+    # Prematurely break if the user deselects or cancels the operation. 
+    if not filepath:
+        return
+
+    # Writes the file and the desired destination.
+    with open(filepath, 'w') as output_file:
+        text = txt.get(1.0, tk.END)
+        output_file.write(text)
+
+# Creates the window.
 window = tk.Tk()
-window.title('Train Station Viewer')
-window.rowconfigure(0, minsize=600, weight=1)
-window.columnconfigure(1, minsize=300, weight=1)
+window.title(f'Train Station Viewer')
+window.rowconfigure(0, minsize=800, weight=1)
+window.columnconfigure(1, minsize=600, weight=1)
 
-frm_entry = tk.Frame(master=window)
+# Text field.
+txt = tk.Text(window, state='disabled')
 
-ent = tk.Entry(master=frm_entry, width=50)
-lbl = tk.Label(master=frm_entry, text='Search')
+# Drop shadows.
+fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+fr_dropdown = tk.Frame(window, relief=tk.RAISED, bd=2)
 
-variable = StringVar(master=frm_entry)
-variable.set('test')
-array = ['test', 'Test', 'Hello', 'World!']
-w = tk.OptionMenu(master=frm_entry, variable, *array)
-w.pack()
+# Buttons.
+btn_open = tk.Button(fr_buttons, text='Search', command=search)
+btn_save = tk.Button(fr_buttons, text='Save as .txt', command=save)
 
-ent.grid(row=0, column=0, sticky='e')
-lbl.grid(row=0, column=1, sticky='w')
+# Dropdown.
+state = tk.StringVar()
+menu = tk.OptionMenu(window, state, 'Karlstad', 'Avrika')
+menu.pack()
 
+# Organises all of the UI elements.
+btn_open.grid(row=0, column=0, sticky='ew', padx=5, pady=5)
+btn_save.grid(row=1, column=0, sticky='ew', padx=5)
+menu.grid(row=2, column=1, sticky='ew', padx=5, pady=5)
+fr_buttons.grid(row=0, column=0, sticky='ns')
+fr_dropdown.grid(row=2, column=1, sticky='ns')
+txt.grid(row=0, column=1, sticky='nsew')
 
-# Create the conversion Button and result display Label
-btn = tk.Button(
-    master=window,
-    text='Export as JSON',
-)
-
-# Set-up the layout using the .grid() geometry manager
-frm_entry.grid(row=0, column=0, padx=10)
-btn.grid(row=0, column=1, pady=10)
-lbl.grid(row=0, column=2, padx=10)
-lbl.grid(row=1, column=0, padx=10)
-
-# Run the application
 window.mainloop()
